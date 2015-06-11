53ab329c2decc0858eec3c65a92b907a3e47f70f
hsusmita <susmita.horrow@gmail.com>
2015-06-11 18:39:17 +0530

    Initial project added

---
+++ XMPPChatRoom/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XMPPChatRoom
+//
+//  Created by Susmita Horrow on 6/11/15.
+//  Copyright (c) 2015 Susmita Horrow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XMPPChatRoom/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XMPPChatRoom
+//
+//  Created by Susmita Horrow on 6/11/15.
+//  Copyright (c) 2015 Susmita Horrow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
